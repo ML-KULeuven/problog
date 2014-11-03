1942cd7b93cdfa6a9ef62243e5ed53c2ab24c504
Anton Dries <anton.dries@cs.kuleuven.be>
2014-11-03 09:26:50 +0100

    Remove obsolete test (code being tested has been removed).

--- problog/logic/test_prolog.py
+++ problog/logic/test_prolog.py
@@ -1,20 +1,20 @@
-import unittest
-from .basic import *
-from .prolog import *
-
-class TestLogicProlog(unittest.TestCase) :
-    
-    def test_term_eq_term_plus(self) :
-        
-        f = Term("'+'", Constant(2), Constant(3))
-        
-        self.assertEqual(compute(f), Constant(5) )
-    
-    def test_term_eq_term_times(self) :
-        f = Term("'*'", Constant(2), Constant(3))
-    
-        self.assertEqual(compute(f), Constant(6) )
-        
-if __name__ == '__main__' :
-    unittest.main(verbosity=2)
+# import unittest
+# from .basic import *
+# from .prolog import *
+#
+# class TestLogicProlog(unittest.TestCase) :
+#
+#     def test_term_eq_term_plus(self) :
+#
+#         f = Term("'+'", Constant(2), Constant(3))
+#
+#         self.assertEqual(compute(f), Constant(5) )
+#
+#     def test_term_eq_term_times(self) :
+#         f = Term("'*'", Constant(2), Constant(3))
+#
+#         self.assertEqual(compute(f), Constant(6) )
+#
+# if __name__ == '__main__' :
+#     unittest.main(verbosity=2)
         
