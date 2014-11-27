ec05993e1680d775e0f13059da1bb663a224683f
Anton Dries <anton.dries@gmail.com>
2014-11-27 19:37:37 +0100

    Fix imports

--- problog/engine.py
+++ problog/engine.py
@@ -1,9 +1,7 @@
 from __future__ import print_function
 
-from __future__ import print_function
-
-from .logic import Term, Constant
 from .program import ClauseDB, PrologString
+from .logic import Term, Constant
 from .formula import LogicFormula
 
 """
