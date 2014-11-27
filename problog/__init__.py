f11a9e0c077a242538e4d265389f4cbb6a637eb4
Anton Dries <anton.dries@cs.kuleuven.be>
2014-11-27 19:01:35 +0100

    Remove package forwards.

--- problog/__init__.py
+++ problog/__init__.py
@@ -1,10 +1,4 @@
 
-from .program import PrologString, PrologFile
-
-from .interface import ground
-from .evaluator import Evaluator
-
-
 # Set the PATH and PYTHON_PATH variables
 from .setup import set_environment
 set_environment()
