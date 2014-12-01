0f64a51869295f8a2a0718360ba2fc8e03b49a3c
Anton Dries <anton.dries@cs.kuleuven.be>
2014-12-01 13:29:21 +0100

    Add root_path() function.

--- problog/__init__.py
+++ problog/__init__.py
@@ -1,4 +1,9 @@
+import os
 
 # Set the PATH and PYTHON_PATH variables
 from .setup import set_environment
 set_environment()
+
+def root_path(*args) :
+    return os.path.abspath( os.path.join( os.path.dirname(__file__), '..', *args ) )
+    
