47ad60a316ac0d9da4c66d228a8aba43ad8db434
Anton Dries <anton.dries@cs.kuleuven.be>
2014-12-01 13:38:10 +0100

    Remove install() from NNF based tests.

--- problog/test/test_system.py
+++ problog/test/test_system.py
@@ -24,8 +24,6 @@ class TestSystemNNF(unittest.TestCase) :
     
     def setUp(self) :
         
-        install()
-        
         try :
             self.assertSequenceEqual = self.assertItemsEqual
         except AttributeError :
