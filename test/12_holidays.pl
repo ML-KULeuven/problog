1fa8ff4543348abd6f1f6fa98c380cfba3ad22a8
Anton Dries <anton.dries@cs.kuleuven.be>
2014-12-07 13:39:46 +0100

    Update test 12 to use non-ground query.

--- test/12_holidays.pl
+++ test/12_holidays.pl
@@ -16,14 +16,10 @@ destinations(city,seaside,mountains).
 
 0.7::goes_to(X,D1,T); 0.15::goes_to(X,D2,T); 0.15::goes_to(X,D3,T) <- T>0, TPrev is T-1, destinations(D1,D2,D3), goes_to(X,D1,TPrev).
 
-query(goes_to(alice,mountains,1)).
-query(goes_to(alice,seaside,1)).
-query(goes_to(alice,city,1)).
+query(goes_to(alice,_,1)).
 
 0.4::gt(P,seaside,0); 0.3::gt(P,mountains,0); 0.3::gt(P,city,0) <- person(P).
 
 0.7::gt(X,D1,T); 0.15::gt(X,D2,T); 0.15::gt(X,D3,T) <- T>0, TPrev is T-1, gt(X,D1,TPrev), destinations(D1,D2,D3).
 
-query(gt(alice,mountains,1)).
-query(gt(alice,seaside,1)).
-query(gt(alice,city,1)).
+query(gt(alice,_,1)).
