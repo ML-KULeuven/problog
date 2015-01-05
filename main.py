2da21c7812549092dbdc369fdb6364c68c62b6cb
Anton Dries <anton.dries@cs.kuleuven.be>
2015-01-05 10:17:06 +0100

    Remove web option from main and update README.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from problog.evaluator import SemiringSymbolic, Evaluator
 from problog.nnf_formula import NNF
 from problog.sdd_formula import SDD
 
-def print_result_default( d, output, precision=8 ) :    
+def print_result( d, output, precision=8 ) :    
     success, d = d
     if success :
         l = max( len(k) for k in d )
@@ -25,16 +25,6 @@ def print_result_default( d, output, precision=8 ) :
         print ('Error:', d, file=output)
         return 1
             
-def print_result_web( d, output, precision=8 ) :
-    success, d = d
-    if success :
-        import json
-        print (200, 'application/json', json.dumps(d), file=output)
-    else :
-        print (400, 'text/plain', d, file=output)
-    return 0 
-    
-
 def process_error( err ) :
     """Take the given error raise by ProbLog and produce a meaningful error message."""
     err_type = type(err).__name__
@@ -63,22 +53,14 @@ def main( filename, knowledge=NNF, semiring=None ) :
 if __name__ == '__main__' :
     
     import argparse
-    
-    output_formats = { 
-        'default' : print_result_default,
-        'web' : print_result_web
-    }
-    
+        
     parser = argparse.ArgumentParser()
     parser.add_argument('filenames', metavar='MODEL', nargs='+')
     parser.add_argument('--knowledge', '-k', choices=('sdd','nnf'), default='nnf', help="Knowledge compilation tool.")
     parser.add_argument('--symbolic', action='store_true', help="Use symbolic evaluation.")
-    parser.add_argument('--output-format', choices=output_formats.keys(), default='default', help="Output format.")
     parser.add_argument('--output', '-o', help="Output file (default stdout)")
     
     args = parser.parse_args()
-    
-    print_result = output_formats[ args.output_format ]
     
     if args.output == None :
         output = sys.stdout
