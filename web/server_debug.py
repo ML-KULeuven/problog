f313abf6a3b00c94d57d072cb72398bd48f5f4dd
Anton Dries <anton.dries@cs.kuleuven.be>
2015-01-06 11:03:47 +0100

    Web: simplified server code

--- web/server_debug.py
+++ web/server_debug.py
@@ -1,25 +1,3 @@
-"""Simple HTTP server for ProbLog.
-
-This module only defines the server interface.
-To change settings related to ProbLog, you should consult the run_problog.py script.
-
-The operation of the server is determined by four options:
-
-    port (default: 8000)        Server port to use
-    timeout (default: 60)       Maximum *processing* time of the ProbLog subprocess
-    memout (default: 1Gb)       Maximum memory usage of the ProbLog subprocess
-    servefiles (default: No)    Whether to serve a file for undefined paths. (This is potentially unsafe.)
-    
-The server defines one path:
-
-    http://hostname:port/problog?model=...
-    
-This path can be used with GET or POST requests.
-
-If ``servefiles`` is enabled other paths will be treated as file access requests.
-
-"""
-
 from __future__ import print_function
 
 # Load general Python modules
@@ -31,11 +9,20 @@ import traceback
 import subprocess
 import resource
 
+# Load ProbLog modules
+sys.path.insert(0, os.path.abspath( os.path.join( os.path.dirname(__file__), '..' ) ) )
+from problog.program import PrologString
+from problog.evaluator import SemiringSymbolic, Evaluator
+from problog.nnf_formula import NNF
+from problog.sdd_formula import SDD
+from problog.formula import LogicDAG, LogicFormula
+
+# Which compiled knowledge format to use? (SDD or NNF)
+KNOWLEDGE = NNF
+
 DEFAULT_PORT = 8000
 DEFAULT_TIMEOUT = 60
 DEFAULT_MEMOUT = 1.0 # gigabyte
-
-SERVE_FILES=False
 
 
 # Load Python standard web-related modules (based on Python version)
@@ -107,6 +94,7 @@ def call_process( cmd, timeout, memout ) :
 def run_problog( model ) :
     """Evaluate the given model and return the probabilities."""
     model = model[0]
+    knowledge = KNOWLEDGE
     
     handle, tmpfile = tempfile.mkstemp('.pl')
     with open(tmpfile, 'w') as f :
@@ -126,6 +114,74 @@ def run_problog( model ) :
     except subprocess.CalledProcessError :
         return 500, 'text/plain', 'ProbLog evaluation exceeded time or memory limit'
     
+    # try :
+    #     formula = knowledge.createFrom( PrologString(model) )
+    #     result = formula.evaluate()
+    #     return 200, 'application/json', json.dumps(result)
+    # except Exception as err :
+    #     return process_error(err)
+
+@handle_url('/ground')
+def run_ground( model ) :
+    """Ground the program given by model and return an SVG of the resulting formula."""
+    model = model[0]
+    knowledge = LogicFormula
+    
+    #from problog.engine import EngineLogger, SimpleEngineLogger
+    #EngineLogger.setClass(SimpleEngineLogger)
+    
+    try :
+
+        formula = knowledge.createFrom( PrologString(model) )
+        
+        handle, filename = tempfile.mkstemp('.dot')
+        with open(filename, 'w') as f :
+            f.write(formula.toDot())     
+        result = subprocess.check_output(['dot', '-Tsvg', filename]).decode('utf-8')
+        content_type = 'application/json'
+        #EngineLogger.setClass(None)
+        return 200, content_type, json.dumps({ 'svg' : result, 'txt' : str(formula) })
+    except Exception as err :
+        #EngineLogger.setClass(None)
+        return process_error(err)
+
+def process_error( err ) :
+    """Take the given error raise by ProbLog and produce a meaningful error message."""
+    err_type = type(err).__name__
+    if err_type == 'ParseException' :
+        return 400, 'text/plain', 'Parsing error on %s:%s: %s.\n%s' % (err.lineno, err.col, err.msg, err.line )
+    elif err_type == 'UnknownClause' :
+        return 400, 'text/plain', 'Predicate undefined: \'%s\'.' % (err )
+    elif err_type == 'PrologInstantiationError' :
+        return 400, 'text/plain', 'Arithmetic operation on uninstantiated variable.' 
+    elif err_type == 'UnboundProgramError' :
+        return 400, 'text/plain', 'Unbounded program or program too large.'
+    else :
+        traceback.print_exc()
+        return 400, 'text/plain', 'Unknown error: %s' % (err_type)
+
+@handle_url('/models')    
+def list_models( ) :
+    
+    def extract_name(f) :
+        return os.path.splitext(os.path.basename(f))[0]
+    
+    files = map(extract_name,glob.glob(model_path('*.pl')))
+    
+    return 200, 'application/json', json.dumps(files)
+    
+    
+@handle_url('/model')
+def load_model( name ) :
+    name = name[0]
+    filename = model_path(name + '.pl')
+    try :
+        with open(filename) as f :
+            data = f.read()
+        return 200, 'text/plain', data
+    except Exception :
+        return 404, 'text/plain', 'File not found!'
+
 
 class ProbLogHTTP(BaseHTTPServer.BaseHTTPRequestHandler) :
           
@@ -151,12 +207,7 @@ class ProbLogHTTP(BaseHTTPServer.BaseHTTPRequestHandler) :
         
         action = PATHS.get(path)
         if action == None :
-            if SERVE_FILES :
-                self.serveFile(path)
-            else :
-                self.send_response(404)
-                self.end_headers()
-                self.wfile.write(toBytes('File not found!'))  
+            self.serveFile(path)
         else :
             code, datatype, data = action( **query )
             self.send_response(code)
@@ -194,12 +245,10 @@ if __name__ == '__main__' :
     parser.add_argument('--port', '-p', type=int, default=DEFAULT_PORT, help="Server listening port")
     parser.add_argument('--timeout', '-t', type=int, default=DEFAULT_TIMEOUT, help="Time limit in seconds")
     parser.add_argument('--memout', '-m', type=float, default=DEFAULT_MEMOUT, help="Memory limit in Gb")
-    parser.add_argument('--servefiles', '-F', action='store_true', help="Attempt to serve a file for undefined paths (unsafe?).")
     args = parser.parse_args(sys.argv[1:])
         
     DEFAULT_TIMEOUT = args.timeout
     DEFAULT_MEMOUT = args.memout
-    SERVE_FILES = args.servefiles
     print ('Starting server on port %d (timeout=%d, memout=%dGb)' % (args.port, DEFAULT_TIMEOUT, DEFAULT_MEMOUT ))    
     
     server_address = ('', args.port)
