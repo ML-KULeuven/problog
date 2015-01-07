cc5a996931eb36411b689431b24cb740b945a714
Anton Dries <anton.dries@cs.kuleuven.be>
2015-01-07 10:17:25 +0100

    Add simple GUI.

---
+++ problog-gui.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python
+
+# Intialize ProbLog search paths (necessary to locate guirunner)
+import problog
+    
+import guirunner
+guirunner.show_gui('problog-cli.py', 'ProbLog v2.1')
