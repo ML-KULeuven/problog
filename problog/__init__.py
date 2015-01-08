8ac27ac4c71920e05bf94532e3588c074f8cd175
Anton Dries <anton.dries@cs.kuleuven.be>
2015-01-08 11:48:12 +0100

    Import submodules in problog package (makes sure all transformations are loaded and allows doing e.g. problog.program.PrologFile after importing problog).

--- problog/__init__.py
+++ problog/__init__.py
@@ -8,5 +8,20 @@ system_info = gather_info()
 
 def root_path(*args) :
     return os.path.abspath( os.path.join( os.path.dirname(__file__), '..', *args ) )
-    
+
+# Load all submodules. This has two reasons:
+#   - initializes all transformations (@transform)
+#   - makes it possible to just import 'problog' and then use 
+#       something like problog.program.PrologFile
+from . import cnf_formula    
+from . import core
+from . import engine
+from . import evaluator
+from . import formula
 from . import interface
+from . import logic
+from . import nnf_formula
+from . import parser
+from . import program
+from . import sdd_formula
+from . import util
