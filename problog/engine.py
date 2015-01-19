febbca2cc760d088c5d387aa52e7214f48dd7f99
Anton Dries <anton.dries@cs.kuleuven.be>
2015-01-19 21:30:56 +0100

    Fix import for Python 3

--- problog/engine.py
+++ problog/engine.py
@@ -1054,4 +1054,4 @@ class SimpleEngineLogger(EngineLogger) :
         print (type(source).__name__, id(source), 'connect', type(listener).__name__, id(listener))
         
         
-from engine_alt import StackBasedEngine as DefaultEngine
+from .engine_alt import StackBasedEngine as DefaultEngine
