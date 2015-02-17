90409233e76f337d990ce05561d62940388f5756
Anton Dries <anton.dries@cs.kuleuven.be>
2015-02-17 15:27:18 +0100

    Make py2pl and pl2py handle more cases.

--- problog/pypl.py
+++ problog/pypl.py
@@ -4,11 +4,18 @@ from .logic import *
 
 def py2pl(d):
     """Translate a given Python datastructure into a Prolog datastructure."""
-
+    
     if type(d) == list or type(d) == tuple:
-        tail = Term('.', py2pl(d[-1]), Term('[]'))
+        if type(d) == tuple :
+            f = ','
+            tail = py2pl(d[-1])
+        else :
+            f = '.'
+            if not d :
+                return Term('[]')
+            tail = Term(f, py2pl(d[-1]), Term('[]'))
         for el in reversed(d[:-1]):
-            tail = Term('.', py2pl(el), tail)
+            tail = Term(f, py2pl(el), tail)
         return tail
 
     if type(d) == str:
@@ -16,6 +23,9 @@ def py2pl(d):
 
     if type(d) == int or type(d) == float:
         return Constant(d)
+        
+    if type(d) == Term :
+        return d
 
     raise Exception("Cannot convert from Python to Prolog: {} ({}).".format(d, type(d)))
     return None
@@ -23,12 +33,12 @@ def py2pl(d):
 
 def pl2py(d):
     """Translate a given Prolog datastructure into a Python datastructure."""
-
+    
     if isinstance(d, Constant):
         if type(d.value) == str:
             return d.value.replace('"','')
         return d.value
-
+        
     if isinstance(d, Term):
         if d.functor == "." and d.arity == 2:
             # list
@@ -40,6 +50,18 @@ def pl2py(d):
             if str(tail) != '[]':
                 elements.append(pl2py(tail))
             return elements
+        elif d.functor == "," and d.arity == 2:
+            # list
+            elements = []
+            tail = d
+            while isinstance(tail,Term) and tail.arity == 2 and tail.functor == ',' :
+                elements.append(pl2py(tail.args[0]))
+                tail = tail.args[1]
+            if str(tail) != '[]':
+                elements.append(pl2py(tail))
+            return elements
+        elif d.arity == 0 :
+            return d.functor
 
     raise Exception("Cannot convert from Prolog to Python: {} ({}).".format(d, type(d)))
     return None
