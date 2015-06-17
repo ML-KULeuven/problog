45014053529ee6a744ec24f002447d3ac301dbed
Anton Dries <anton.dries@cs.kuleuven.be>
2015-06-17 09:18:33 +0200

    MPE: switch to SCIP as backend with LP format.

--- mpe/mpe.py
+++ mpe/mpe.py
@@ -9,6 +9,7 @@ from __future__ import print_function
 
 import math
 import subprocess
+import sys
 
 from problog.program import PrologFile
 from problog.formula import LogicFormula
@@ -53,7 +54,7 @@ def groundprogram2flatzinc(gp):
     else:
         relevant = extract_relevant(gp)
 
-    print ('Relevant program size:', sum(relevant))
+    print ('Relevant program size:', sum(relevant), file=sys.stderr)
 
     atom_vars = []
     atoms = []
@@ -140,8 +141,148 @@ def groundprogram2flatzinc(gp):
     solve = 'solve :: bool_search([%s], occurrence, indomain_max, complete) maximize %s;' % (','.join(atom_vars), optvar)
     return '\n'.join(variables) + '\n' + '\n'.join(constraints) + '\n' + solve
 
+def groundprogram2lp(gp):
 
-def call_solver(fzn, solver):
+    # b = a_1 /\ a_2 /\ ... /\ a_n
+    #
+    #   => 0 <= a_1 + a_2 + ... + a_n - nb < n
+    #
+    # b = a_1 \/ a_2 \/ ... \/ a_n
+    #
+    #   -b = -a_1 /\ -a2 /\ ... /\ -a_n
+    #
+    #   => 0 <= -a_1 - a_2 ... - a_n + nb < n
+
+    variables = []
+    constraints = []
+
+    weights = gp.extractWeights(SemiringLogProbability())
+
+    has_constraint = False
+    for c in gp.constraints():
+        has_constraint = True
+        break
+
+    if has_constraint:
+        relevant = [True] * (len(gp)+1)
+    else:
+        relevant = extract_relevant(gp)
+
+    print ('Relevant program size:', sum(relevant), file=sys.stderr)
+
+    atom_vars = []
+    bounds = []
+    atoms = []
+    probs = []
+    probs_offset = 0.0
+    neg_nodes = set()
+    for i, n, t in gp:
+        if not relevant[i]:
+            continue
+        varname = 'x%s' % i     # Variable name corresponding to this node
+        bounds.append('0 <= x%s <= 1' % i)
+        if t == 'conj':
+            formula = ''
+            low_bound = 0
+            high_bound = len(n.children)
+            for c in n.children:
+                if c < 0:
+                    low_bound -= 1
+                    high_bound -= 1
+                    formula += '-x%s' % -c
+                else:
+                    if formula:
+                        formula += '+'
+                    formula += 'x%s' % c
+            constraints.append(formula + ' >= ' + str(low_bound))
+            constraints.append(formula + ' < ' + str(high_bound))
+        elif t == 'disj':
+            formula = ''
+            low_bound = 0
+            high_bound = len(n.children)
+            for c in n.children:
+                if c < 0:
+                    low_bound += 1
+                    high_bound += 1
+                    if formula:
+                        formula += '+'
+                    formula += 'x%s' % -c
+                else:
+                    formula += '-x%s' % c
+            constraints.append(formula + ' >= ' + str(low_bound))
+            constraints.append(formula + ' < ' + str(high_bound))
+        else:
+            pp, pn = weights[i]
+            atom_vars.append(varname)
+            atoms.append(varname)
+            # atoms.append(var_float_neg)
+            probs.append(str(pp-pn))
+            # probs.append(str(-pn))
+            probs_offset -= pn
+
+    obj = []
+    for at, pr in zip(probs, atoms):
+        obj.append('%s %s' % (at, pr))
+    obj = ' + '.join(obj)
+
+    for q, n in gp.queries():
+        constraints.append('x%s > 0' % n)
+
+    for q, n in gp.evidence():
+        if n > 0:
+            constraints.append('x%s > 0;' % n)
+        else:
+            constraints.append('x%s = 0;' % n)
+
+    # TODO constraints
+    # for c in gp.constraints():
+    #     for r in c.encodeCNF():
+    #         r_pos = []
+    #         r_neg = []
+    #         for x in r:
+    #             if x > 0:
+    #                 r_pos.append('b_lit_%s_pos' % x)
+    #             else:
+    #                 r_neg.append('b_lit_%s_pos' % -x)
+    #         constraints.append('constraint bool_clause([%s],[%s]);' % (','.join(r_pos),','.join(r_neg)))
+
+    result = """
+maximize
+  obj: %s
+subject to
+  %s
+bounds
+  %s
+binary
+  %s
+end
+""" % (obj, '\n  '.join(constraints), '\n  '.join(bounds), ' '.join(atom_vars))
+
+    return result, probs_offset
+
+def call_scip(lp, solver):
+    filename = '/tmp/mpe.lp'
+    with open(filename, 'w') as f:
+        f.write(lp)
+
+    output = subprocess.check_output(solver.split(' ') + ['-f', filename])
+
+    obj_value = None
+    facts = set()
+
+    in_the_zone = False
+    for line in output.split('\n'):
+        line = line.strip()
+        if line.startswith('objective value'):
+            in_the_zone = True
+            obj_value = float(line.split()[2])
+        elif in_the_zone:
+            if not line:
+                return obj_value, facts
+            else:
+                facts.add(int(line.split()[0][1:]))
+
+def call_flatzinc_solver(fzn, solver, **kwdargs):
 
     filename = '/tmp/mpe.fzn'
     with open(filename, 'w') as f:
@@ -170,23 +311,39 @@ def main(inputfile, **kwdargs):
     pl = PrologFile(inputfile)
     gp = LogicFormula.createFrom(pl, label_all=True)
 
-    flatzinc = groundprogram2flatzinc(gp)
-    score, facts = call_solver(flatzinc, **kwdargs)
-    for name, node in gp.getNames():
-        val = facts.get(node)
-        if val == True:
-            del facts[node]
-            print (name)
-        elif val == False:
-            del facts[node]
-            print ('\+' + str(name))
-    for f, v in facts.items():
-        if v:
-            print ('node_%s' % f)
-        else:
-            print ('\+node_%s' % f)
+    lp, score_offset = groundprogram2lp(gp)
 
-    print ('Probability:', score)
+    score, facts = call_scip(lp, **kwdargs)
+    score -= score_offset
+
+    for name, node in gp.getNames():
+        if node in facts:
+            facts.remove(node)
+            print (name)
+    for f in facts:
+        print ('node_%s' % f)
+
+    print ('Probability:', math.exp(score))
+
+
+
+    # flatzinc = groundprogram2flatzinc(gp)
+    # score, facts = call_solver(flatzinc, **kwdargs)
+    # for name, node in gp.getNames():
+    #     val = facts.get(node)
+    #     if val == True:
+    #         del facts[node]
+    #         print (name)
+    #     elif val == False:
+    #         del facts[node]
+    #         print ('\+' + str(name))
+    # for f, v in facts.items():
+    #     if v:
+    #         print ('node_%s' % f)
+    #     else:
+    #         print ('\+node_%s' % f)
+    #
+    # print ('Probability:', score)
 
 
 def argparser():
@@ -194,7 +351,7 @@ def argparser():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('inputfile')
-    parser.add_argument('--solver', type=str, default='fzn-gecode', help="FlatZinc solver")
+    parser.add_argument('--solver', type=str, default='scip', help="location of the backend solver")
     return parser
 
 
