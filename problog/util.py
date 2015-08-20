2b948ac6da3c7580ec6819c036a4ca6fb677a8fc
Anton Dries <anton.dries@cs.kuleuven.be>
2015-08-20 11:59:04 +0200

    problog.util: Clean up and document.

--- problog/util.py
+++ problog/util.py
@@ -26,10 +26,21 @@ import os
 import subprocess
 import distutils.spawn
 import tempfile
+import imp
+import collections
 
 
-def init_logger(verbose=None):
-    logger = logging.getLogger('problog')
+def init_logger(verbose=None, name='problog'):
+    """Initialize default logger.
+
+    :param verbose: verbosity level (0: WARNING, 1: INFO, 2: DEBUG)
+    :type verbose: int
+    :param name: name of the logger (default: problog)
+    :type name: str
+    :return: result of ``logging.getLogger(name)''
+    :rtype: logging.Logger
+    """
+    logger = logging.getLogger(name)
     ch = logging.StreamHandler(sys.stdout)
     formatter = logging.Formatter('[%(levelname)s] %(message)s')
     ch.setFormatter(formatter)
@@ -44,34 +55,67 @@ def init_logger(verbose=None):
         logger.debug('Output level: DEBUG')
     return logger
 
-class Timer(object) :
 
-    def __init__(self, msg, output=None) :
+class Timer(object):
+    """Report timing information for a block of code.
+    To be used as a ``with'' block.
+
+    :param msg: message to print
+    :type msg: str
+    :param output: file object to write to (default: write to logger ``problog'')
+    :type output: file
+    """
+
+    def __init__(self, msg, output=None):
         self.message = msg
         self.start_time = None
         self.output = output
 
-    def __enter__(self) :
+    def __enter__(self):
         self.start_time = time.time()
 
-    def __exit__(self, *args) :
+    def __exit__(self, *args):
         if self.output is None:
             logger = logging.getLogger('problog')
             logger.info('%s: %.4fs' % (self.message, time.time()-self.start_time))
         else:
             print ('%s: %.4fs' % (self.message, time.time()-self.start_time), file=self.output)
 
-def raise_timeout(*args) :
+
+def _raise_timeout(*args):
+    """Raise global timeout exception (used by global timer)
+
+    :param args: signal information (ignored)
+    :raise KeyboardInterrupt:
+    """
     raise KeyboardInterrupt('Timeout')   # Global exception on all threads
 
-def start_timer(timeout=0) :
-    signal.signal(signal.SIGALRM, raise_timeout)
+
+def start_timer(timeout=0):
+    """Start a global timeout timer.
+
+    :param timeout: timeout in seconds
+    :type timeout: int
+    """
+    signal.signal(signal.SIGALRM, _raise_timeout)
     signal.alarm(timeout)
 
-def stop_timer() :
+
+def stop_timer():
+    """Stop the global timeout timer."""
     signal.alarm(0)
 
-def subprocess_check_call(*popenargs, **kwargs) :
+
+def subprocess_check_call(*popenargs, **kwargs):
+    """Wrapper for subprocess.check_call that recursively kills subprocesses when Python is \
+    interrupted.
+
+    Additionally expands executable name to full path.
+
+    :param popenargs: positional arguments of subprocess.call
+    :param kwargs: keyword arguments of subprocess.call
+    :return: result of subprocess.call
+    """
     retcode = subprocess_call(*popenargs, **kwargs)
     if retcode:
         cmd = kwargs.get("args")
@@ -80,13 +124,19 @@ def subprocess_check_call(*popenargs, **kwargs) :
         raise subprocess.CalledProcessError(retcode, cmd)
     return 0
 
-def find_process(cmd, *rest) :
-    cmd[0] = distutils.spawn.find_executable(cmd[0])
-    return (cmd,) + rest
 
 def subprocess_call(*popenargs, **kwargs):
-    try :
-        popenargs = find_process(*popenargs)
+    """Wrapper for subprocess.call that recursively kills subprocesses when Python is interrupted.
+
+    Additionally expands executable name to full path.
+
+    :param popenargs: positional arguments of subprocess.call
+    :param kwargs: keyword arguments of subprocess.call
+    :return: result of subprocess.call
+    """
+    process = None
+    try:
+        popenargs = _find_process(*popenargs)
         process = subprocess.Popen(*popenargs, **kwargs)
         return process.wait()
     except KeyboardInterrupt:
@@ -97,10 +147,22 @@ def subprocess_call(*popenargs, **kwargs):
         raise
 
 
+def _find_process(cmd, *rest):
+    fullname = distutils.spawn.find_executable(cmd[0])
+    if fullname is not None:
+        return ([fullname] + cmd[1:],) + rest
+    else:
+        return (cmd,) + rest
+
+
 def kill_proc_tree(process, including_parent=True):
-    """
-    Recursively kill a subprocess. Useful when the subprocess is a script.
+    """Recursively kill a subprocess. Useful when the subprocess is a script.
     Requires psutil but silently fails when it is not present.
+
+    :param process: process
+    :type process: subprocess.Popen
+    :param including_parent: also kill process itself (default: True)
+    :type including_parent: bool
     """
     try:
         import psutil
@@ -114,11 +176,16 @@ def kill_proc_tree(process, including_parent=True):
             parent.kill()
             parent.wait(5)
     except ImportError:
+        psutil = None
         process.kill()
 
-import collections
 
 class OrderedSet(collections.MutableSet):
+    """Provides an ordered version of a set which keeps elements in the order they are added.
+
+    :param iterable: add elements from this iterable (default: None)
+    :type iterable: Sequence
+    """
 
     def __init__(self, iterable=None):
         self.end = end = []
@@ -179,7 +246,107 @@ class OrderedSet(collections.MutableSet):
         return set(self) == set(other)
 
 
-def mktempfile(suffix):
+def mktempfile(suffix=''):
+    """Create a temporary file with the given name suffix.
+
+    :param suffix: extension of the file
+    :type suffix: str
+    :return: name of the temporary file
+    """
     fd, filename = tempfile.mkstemp(suffix)
     os.close(fd)
     return filename
+
+
+def load_module(filename):
+    """Load a Python module from a filename or qualified module name.
+
+    If filename ends with ``.py'', the module is loaded from the given file.
+    Otherwise it is taken to be a module name reachable from the path.
+
+    Example:
+
+    .. code-block: python
+
+       pb_util = load_module('problog.util')
+       pb_util = load_module('problog/util.py')
+
+    :param filename: location of the module
+    :type filename: str
+    :return: loaded module
+    :rtype: module
+    """
+    if filename.endswith('.py'):
+        filename = os.path.abspath(os.path.join(os.path.dirname(__file__), filename))
+        (path, name) = os.path.split(filename)
+        (name, ext) = os.path.splitext(name)
+        (extfile, filename, data) = imp.find_module(name, [path])
+        return imp.load_module(name, extfile, filename, data)
+    else:
+        mod = __import__(filename)
+        components = filename.split('.')
+        for c in components[1:]:
+            mod = getattr(mod, c)
+        return mod
+
+
+def format_value(data, precision=8):
+    """Pretty print a given value.
+
+    :param data: data to format
+    :param precision: max. number of digits
+    :type precision: int
+    :return: pretty printed result
+    :rtype: str
+    """
+    if isinstance(data, float):
+        data = ('%.' + str(precision) + 'g') % data
+    return ('{:<' + str(precision+2) + '}').format(data)
+
+
+def format_tuple(data, precision=8, columnsep='\t'):
+    """Pretty print a given tuple (or single value).
+
+    :param data: data to format
+    :param precision: max. number of digits
+    :type precision: int
+    :param columnsep: column separator
+    :type columnsep: str
+    :return: pretty printed result
+    :rtype: str
+    """
+
+    if isinstance(data, str):
+        # is a string -> return string itself
+        return data
+    elif isinstance(data, collections.Sequence):
+        return columnsep.join(map(lambda v: format_value(v, precision=precision), data))
+    else:
+        return format_value(data, precision=precision)
+
+
+def format_dictionary(data, precision=8, keysep=':', columnsep='\t'):
+    """Pretty print a given dictionary.
+
+    :param data: data to format
+    :type data: dict
+    :param precision: max. number of digits
+    :type precision: int
+    :param keysep: separator between key and value (default: ``;'')
+    :type keysep: str
+    :param columnsep: column separator (default: ``tab'')
+    :type columnsep: str
+    :return: pretty printed result
+    :rtype: str
+    """
+    if not data:
+        return ""  # no queries
+    else:
+        s = []
+        # Determine maximum length of key
+        l = max(len(str(k)) for k in data)
+        fmt = ('{:>' + str(l) + '}{}{}{}')
+        for it in sorted([(str(k), v) for k, v in data.items()]):
+            val = format_tuple(it[1], precision=precision, columnsep=columnsep)
+            s.append(fmt.format(it[0], keysep, columnsep, val))
+        return '\n'.join(s)
