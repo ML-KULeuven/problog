0a97f6c34c57a26ba91082be6f7f7fe9244dc64b
Anton Dries <anton.dries@cs.kuleuven.be>
2015-08-24 15:30:50 +0200

    Undo change in test.

--- test/12_holidays.pl
+++ test/12_holidays.pl
@@ -12,14 +12,14 @@ destinations(seaside,mountains,city).
 destinations(mountains,seaside,city).
 destinations(city,seaside,mountains).
 
-% 0.4::goes_to(P,seaside,0); 0.3::goes_to(P,mountains,0); 0.3::goes_to(P,city,0) <- person(P).
-%
-% 0.7::goes_to(X,D1,T); 0.15::goes_to(X,D2,T); 0.15::goes_to(X,D3,T) <- T>0, TPrev is T-1, destinations(D1,D2,D3), goes_to(X,D1,TPrev).
-%
-% query(goes_to(alice,_,1)).
+0.4::goes_to(P,seaside,0); 0.3::goes_to(P,mountains,0); 0.3::goes_to(P,city,0) <- person(P).
+
+0.7::goes_to(X,D1,T); 0.15::goes_to(X,D2,T); 0.15::goes_to(X,D3,T) <- T>0, TPrev is T-1, destinations(D1,D2,D3), goes_to(X,D1,TPrev).
+
+query(goes_to(alice,_,1)).
 
 0.4::gt(P,seaside,0); 0.3::gt(P,mountains,0); 0.3::gt(P,city,0) <- person(P).
 
 0.7::gt(X,D1,T); 0.15::gt(X,D2,T); 0.15::gt(X,D3,T) <- T>0, TPrev is T-1, gt(X,D1,TPrev), destinations(D1,D2,D3).
 
-query(gt(alice,seaside,1)).
+query(gt(alice,_,1)).
