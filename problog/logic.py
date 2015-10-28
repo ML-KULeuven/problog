d49a447a0f7d010e2be1bf9d6f4647fc3355361d
Anton Dries <anton.dries@cs.kuleuven.be>
2015-10-28 10:18:03 +0100

    Version 2.1.0.7
        - Improved hash function for Terms
        - Bugfix: floor/1, round/1, ceiling/1 and truncate/1 now return integers

--- problog/logic.py
+++ problog/logic.py
@@ -555,7 +555,11 @@ class Term(object):
 
     def __hash__(self):
         if self.__hash is None:
-            self.__hash = hash((self.__functor, self.__arity, self._list_length()))
+            firstarg = None
+            if len(self.__args) > 0:
+                firstarg = self.__args[0]
+
+            self.__hash = hash((self.__functor, self.__arity, firstarg, self._list_length()))
         return self.__hash
 
     def __lshift__(self, body):
@@ -859,9 +863,10 @@ _arithmetic_functions = {
     ("float_integer_part", 1): lambda f: int(f),
     ("float_fractional_part", 1): lambda f: f - int(f),
     ("abs", 1): abs,
-    ("ceiling", 1): math.ceil,
-    ("round", 1): round,
-    ("truncate", 1): math.trunc,
+    ("ceiling", 1): lambda x: int(math.ceil(x)),
+    ("round", 1): lambda x: int(round(x)),
+    ("floor", 1): lambda x: int(math.floor(x)),
+    ("truncate", 1): lambda x: int(math.trunc),
     ("min", 2): min,
     ("max", 2): max,
     ("exp", 2): math.pow,
@@ -874,7 +879,7 @@ _arithmetic_functions = {
 
 _from_math_1 = ['exp', 'log', 'log10', 'sqrt', 'sin', 'cos', 'tan', 'asin', 'acos', 'atan',
                 'sinh', 'cosh', 'tanh', 'asinh', 'acosh', 'atanh', 'lgamma', 'gamma', 'erf',
-                'erfc', 'floor']
+                'erfc']
 for _f in _from_math_1:
     _arithmetic_functions[(_f, 1)] = getattr(math, _f)
 
