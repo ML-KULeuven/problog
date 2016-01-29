a39c11bd88e05240c62183da76ca2c17a3991cd4
Anton Dries <anton.dries@cs.kuleuven.be>
2016-01-29 11:45:15 +0100

    Fix test (missing queries).

--- test/extern_test_out.pl
+++ test/extern_test_out.pl
@@ -3,6 +3,13 @@
 % int_plus_times(1,2,3,2) 1
 % int_plus_times(1,2,3,3) 0
 % p 1
+% b(1) 1
+% b(2) 1
+% b(3) 1
+% b(4) 1
+% c(3) 1
+% c(5) 0
+
 :- use_module('extern_lib.py').
 
 query(int_plus(1,2,3)).
