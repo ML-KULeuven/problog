44d190214cb521a7f97ce680ab33a1e24854e2f6
Wannes Meert <wannes.meert@cs.kuleuven.be>
2016-02-14 15:04:54 +0100

    Print evidence causing the inconsistent ev

--- problog/errors.py
+++ problog/errors.py
@@ -73,11 +73,14 @@ class UserError(ProbLogError):
 class InconsistentEvidenceError(ProbLogError):
     """Error when evidence is inconsistent"""
 
-    def __init__(self, source=None):
+    def __init__(self, source=None, evidence=None):
+        explanation = ''
+        if evidence is not None:
+            explanation = ' ({})'.format(evidence)
         if source is None:
-            ProbLogError.__init__(self, "Inconsistent evidence detected")
+            ProbLogError.__init__(self, "Inconsistent evidence detected%s" % explanation)
         else:
-            ProbLogError.__init__(self, "Inconsistent evidence detected: '%s'" % source)
+            ProbLogError.__init__(self, "Inconsistent evidence detected%s: '%s'" % explanation, source)
 
 
 def process_error(err, debug=False):
