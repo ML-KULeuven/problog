b4365f87728d4f88869d5512f89e137783c6bb21
Anton Dries <anton.dries@cs.kuleuven.be>
2016-02-29 11:30:09 +0100

    aProbLog: simplify definition: use positive label from model.

--- problog/library/aproblog.py
+++ problog/library/aproblog.py
@@ -4,10 +4,24 @@ from problog.extern import problog_export
 from problog.evaluator import Semiring
 
 
-@problog_export('+str')
-def use_semiring(name):
+@problog_export('+term', '+term', '+term', '+term', '+term', '+str', '+str')
+def use_semiring(plus, times, zero, one, neg, is_dsp, is_nsp):
 
-    problog_export.database.set_data('semiring', SemiringMPEState())
+    class CustomSemiring(Semiring):
+        pass
+
+    CustomSemiring.zero = problog_export.database.create_function(zero, 1)
+    CustomSemiring.one = problog_export.database.create_function(one, 1)
+    CustomSemiring.plus = problog_export.database.create_function(plus, 3)
+    CustomSemiring.times = problog_export.database.create_function(times, 3)
+    CustomSemiring.pos_value = lambda s, x, k: x
+    CustomSemiring.neg_value = problog_export.database.create_function(neg, 2)
+    CustomSemiring.is_dsp = lambda s: is_dsp == 'true'
+    CustomSemiring.is_nsp = lambda s: is_nsp == 'true'
+
+    semiring = CustomSemiring()
+    problog_export.database.set_data('semiring', semiring)
+
     return ()
 
 
