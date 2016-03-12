7dc9869414eeeabb60c8e922727f4f0d9318c631
Wannes Meert <wannes.meert@cs.kuleuven.be>
2016-03-12 17:46:54 +0100

    Disjoint block splitting

--- examples/example_multfact.py
+++ examples/example_multfact.py
@@ -4,38 +4,20 @@
 """
 Example on how to use multiplicative factorization in ProbLog.
 
-Based on:
-- M. Takikawa and B. D'Ambrosio. Multiplicative factorization of noisy-max. In K. B. Laskey and H. Prade, editors, Proceedings of the 15th Conference on Uncertainty in Artificial Intelligence (UAI), pages 622-630, 1999.
-- F. J. Diez and S. F. Galan. Efficient computation for the noisy max. International Journal of Intelligent Systems, 18(2):165-177, 2003.
-- G. Van den Broeck, W. Meert, and A. Darwiche. Skolemization for weighted first-order model counting. In Proceedings of the 14th International Conference on Principles of Knowledge Representation and Reasoning (KR), 2014.
-- W. Meert, G. Van den Broeck, and A. Darwiche. Lifted inference for probabilistic logic programs. In Proceedings of the Workshop on Probabilistic Logic Programming (PLP), pages 1-8, July 2014.
-
-Transformation (following [Van den Broeck 2015]):
-
-… (c1 ∨ c2 ∨ c3) …
-… ⋁ci …
-
-(… z …)
-∧ (z ∨ ¬⋁ci)
-∧ (z ∨ s)
-∧ (s ∨ ¬⋁ci)
-
-(… z …)
-∧ (z ∨ ¬c1)
-∧ (z ∨ ¬c2)
-∧ (z ∨ ¬c3)
-∧ (z ∨ s)
-∧ (s ∨ ¬c1)
-∧ (s ∨ ¬c2)
-∧ (s ∨ ¬c3)
-
-with w(z) = w(¬z) = w(s) = 1 and w(¬s) = -1
+Observations:
+- Only for large (>30) disjunctions, it results in a speedup.
+- For really large (>80) disjunctions, the speedup is an order of magnitude
 """
 
 from __future__ import print_function
 
 import sys, os
 import logging
+import math
+try:
+    from itertools import zip_longest
+except:
+    from itertools import izip_longest as zip_longest
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
@@ -51,29 +33,63 @@ from problog.util import Timer, start_timer, stop_timer, init_logger, format_dic
 from problog.constraint import ClauseConstraint, ConstraintAD
 from problog.evaluator import SemiringProbability, FormulaEvaluator, OperationNotSupported
 
-def multiplicative_factorization(source, or_threshold=8):
+def multiplicative_factorization(source, disjunct_threshold=8, disjunct_max=None):
     """
     Copy the source LogicFormula and replace large disjunctions with Skolemization variables
     to obtain a formula that is similar to the result of multiplicative factorization.
+
+    Based on:
+    - M. Takikawa and B. D'Ambrosio. Multiplicative factorization of noisy-max. In K. B. Laskey and H. Prade, editors, Proceedings of the 15th Conference on Uncertainty in Artificial Intelligence (UAI), pages 622-630, 1999.
+    - F. J. Diez and S. F. Galan. Efficient computation for the noisy max. International Journal of Intelligent Systems, 18(2):165-177, 2003.
+    - G. Van den Broeck, W. Meert, and A. Darwiche. Skolemization for weighted first-order model counting. In Proceedings of the 14th International Conference on Principles of Knowledge Representation and Reasoning (KR), 2014.
+    - W. Meert, G. Van den Broeck, and A. Darwiche. Lifted inference for probabilistic logic programs. In Proceedings of the Workshop on Probabilistic Logic Programming (PLP), pages 1-8, July 2014.
+
+    Transformation with disjunct_threshold<3 and disjunct_max=1 (following [Van den Broeck 2015]):
+
+    … (c1 ∨ c2 ∨ c3) …
+    … ⋁ci …
+
+    (… z …)
+    ∧ (z ∨ ¬⋁ci)
+    ∧ (z ∨ s)
+    ∧ (s ∨ ¬⋁ci)
+
+    (… z …)
+    ∧ (z ∨ ¬c1)
+    ∧ (z ∨ ¬c2)
+    ∧ (z ∨ ¬c3)
+    ∧ (z ∨ s)
+    ∧ (s ∨ ¬c1)
+    ∧ (s ∨ ¬c2)
+    ∧ (s ∨ ¬c3)
+
+    with w(z) = w(¬z) = w(s) = 1 and w(¬s) = -1
+
     :param source: Input formula
+    :param disjunct_threshold: Split disjuncts larger than this threshold
+    :param disjunct_max: Maximal size of new disjuncts (default is disjunct_threshold)
     :return: new formula, additional query atoms
     """
     assert(type(source) == LogicFormula) # Can be relaxed to isinstance
     logger = logging.getLogger('problog')
-    logger.info('Disjunction threshold: {}'.format(or_threshold))
     target = LogicFormula()
     tseitin_vars = []
     skolem_vars = []
     extra_clauses = []
     extra_queries = []
     identifier_prefix = 'MF_'
+    if disjunct_max is None:
+        disjunct_max = disjunct_threshold
+
+    logger.info('Disjunction threshold: {}'.format(disjunct_threshold))
+    logger.info('Disjunction max block size: {}'.format(disjunct_max))
 
     # Copy formulas and replace large disjunctions with a Tseitin variable z_i
     for key,node,t in source:
         logger.debug('Rule: {:<3}: {} -- {}'.format(key,node,t))
         nodetype = type(node).__name__
         if nodetype == 'disj':
-            if len(node.children) > or_threshold:
+            if len(node.children) > disjunct_threshold:
                 # Replace disjunction node with atom node.
                 tseitin_vars.append(Term(identifier_prefix+'z_{}'.format(len(tseitin_vars))))
                 logger.debug('-> {}: replace disj with tseitin {}'.format(key, tseitin_vars[-1]))
@@ -105,13 +121,22 @@ def multiplicative_factorization(source, or_threshold=8):
         target.add_name(skolem_var, skolem, target.LABEL_QUERY)
         extra_queries.append(skolem_var)
         target.add_constraint(ClauseConstraint([tseitin, skolem]))
-        for c in children:
+
+        disjunct_size = math.ceil(len(children)/math.ceil(len(children)/disjunct_max))
+        for c_group in zip_longest(*([iter(children)]*disjunct_size), fillvalue=None):
+            c_group = [c for c in c_group if c is not None]
+            logger.debug('-> Skolem group {}'.format(c_group))
             term = Term(identifier_prefix+'b_{}'.format(cur_body))
-            target.add_name(term, c, target.LABEL_QUERY)
-            extra_queries.append(term)
             cur_body += 1
-            target.add_constraint(ClauseConstraint([tseitin, -c]))
-            target.add_constraint(ClauseConstraint([skolem,  -c]))
+            if len(c_group) == 1:
+                d = c_group[0]
+            else:
+                d = target.add_or(components=c_group, key=None, name=term)
+            target.add_name(term, d, target.LABEL_QUERY)
+            extra_queries.append(term)
+            target.add_constraint(ClauseConstraint([tseitin, -d]))
+            target.add_constraint(ClauseConstraint([skolem,  -d]))
+
 
     # Copy labels
     for name, key, label in source.get_names_with_label():
@@ -202,10 +227,10 @@ class NegativeProbability(SemiringProbability):
         v = float(a)
         return v
 
-def probability(filename, with_fact=True, knowledge='nnf', or_threshold=8):
+def probability(filename, with_fact=True, knowledge='nnf', disjunct_threshold=8, disjunct_max=None):
     logger = logging.getLogger('problog')
     pl = PrologFile(filename)
-    engine = DefaultEngine(label_all=True)#, keep_all=True, keep_duplicates=True)
+    engine = DefaultEngine(label_all=True, keep_all=True, keep_duplicates=True)
     db = engine.prepare(pl)
     gp = engine.ground_all(db)
     # if logger.isEnabledFor(logging.DEBUG):
@@ -218,8 +243,8 @@ def probability(filename, with_fact=True, knowledge='nnf', or_threshold=8):
                 with open('test_f.dot', 'w') as dotfile:
                     print_result((True, gp.to_dot()), output=dotfile)
                 cnf = CNF.createFrom(gp)
-                logger.debug(cnf.to_dimacs())
-            gp2, extra_queries = multiplicative_factorization(gp, or_threshold)
+                # logger.debug(cnf.to_dimacs())
+            gp2, extra_queries = multiplicative_factorization(gp, disjunct_threshold, disjunct_max)
             if logger.isEnabledFor(logging.DEBUG):
                 with open('test_f_mf.dot', 'w') as dotfile:
                     print_result((True, gp2.to_dot()), output=dotfile)
@@ -246,7 +271,7 @@ def probability(filename, with_fact=True, knowledge='nnf', or_threshold=8):
                 with open ('test_f_mf_nnf.dot', 'w') as dotfile:
                     print(nnf.to_dot(), file=dotfile)
                 cnf = CNF.createFrom(gp2)
-                logger.debug(cnf.to_dimacs())
+                # logger.debug(cnf.to_dimacs())
             logger.debug('Deleting queries: {}'.format(extra_queries))
             for query in extra_queries:
                 nnf.del_name(query, nnf.LABEL_QUERY)
@@ -292,8 +317,10 @@ if __name__ == '__main__' :
     parser.add_argument('--verbose', '-v', action='count', help='Verbose output')
     parser.add_argument('--nomf', action='store_true', help='Disable multiplicative factorization')
     parser.add_argument('--profile', action='store_true', help='Profile Python script')
-    parser.add_argument('--threshold', '-t', default=8, type=int,
+    parser.add_argument('--threshold', '-t', default=30, type=int,
                         help='Threshold to break disjunctions')
+    parser.add_argument('--disjunctblocksize', '-b', default=1, type=int,
+                        help='Block size of splits of disjunct')
     parser.add_argument('--knowledge', '-k', default='nnf',
                         help='Knowledge compilation (sdd, nnf, fbdd)')
     args = parser.parse_args()
@@ -302,12 +329,12 @@ if __name__ == '__main__' :
 
     if args.profile:
         import cProfile, pstats
-        cProfile.run('probability( args.filename, not args.nomf, args.knowledge, args.threshold)', 'prstats')
+        cProfile.run('probability( args.filename, not args.nomf, args.knowledge, args.threshold, args.disjunctblocksize)', 'prstats')
         p = pstats.Stats('prstats')
         p.strip_dirs()
         # p.sort_stats('cumulative')
         p.sort_stats('time')
         p.print_stats()
     else:
-        result = probability( args.filename, not args.nomf, args.knowledge, args.threshold)
+        result = probability( args.filename, not args.nomf, args.knowledge, args.threshold, args.disjunctblocksize)
         print_result((True,result), sys.stdout)
