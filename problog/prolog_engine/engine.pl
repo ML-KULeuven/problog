095b1a374f8cf8198e2039dce18f020808eb8d88
Yann Dauxais <yann.dauxais@cs.kuleuven.be>
2019-11-26 12:22:24 +0100

    Remove Robin's leftovers

--- problog/prolog_engine/engine.pl
+++ problog/prolog_engine/engine.pl
@@ -8,13 +8,3 @@ solve((A,B),(ProofA,ProofB)) :- !, solve(A,ProofA), solve(B,ProofB).
 solve(A,A:-ProofB) :- cl(A,B),solve(B,ProofB).
 
 prove(Q,Proofs) :- findall(Proof,solve(Q,Proof),Proofs).
-
-cl(a(1),true).
-cl(b(1),true).
-cl(a(2),true).
-cl(b(2),true).
-cl(d(2),true).
-cl(a(2),true).
-cl(c(X),(a(X),b(X))).
-cl(b(X),d(X)).
-cl(b(X),c(X)).
