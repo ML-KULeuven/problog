b98fb2602b04fc64d80a761817fdf46f41c8b4e5
Thomas Winters <info@thomaswinters.be>
2019-11-26 11:55:26 +0100

    Imported collections.abc

--- problog/lib/pyparsing.py
+++ problog/lib/pyparsing.py
@@ -21,6 +21,7 @@
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
+import collections.abc
 
 __doc__ = """
     pyparsing module - Classes and methods to define and execute parsing grammars
