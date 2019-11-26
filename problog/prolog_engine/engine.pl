e630b80aecd7d810d2171daa211af1782b957f8f
Robin Manhaeve <robin.manhaeve@cs.kuleuven.be>
2019-11-26 13:56:55 +0100

    Auto stash before merge of "new_engine" and "origin/new_engine"

--- problog/prolog_engine/engine.pl
+++ problog/prolog_engine/engine.pl
@@ -6,15 +6,6 @@ solve(true,true) :- !.
 solve((A,B),(ProofA,ProofB)) :- !, solve(A,ProofA), solve(B,ProofB).
 %solve(A,A:-builtin) :- predicate_property(A, builtin), !, A.
 solve(A,A:-ProofB) :- cl(A,B),solve(B,ProofB).
+solve(A,::(P,A)) :- fa(P,A).
 
-prove(Q,Proofs) :- findall(Proof,solve(Q,Proof),Proofs).
-
-cl(a(1),true).
-cl(b(1),true).
-cl(a(2),true).
-cl(b(2),true).
-cl(d(2),true).
-cl(a(2),true).
-cl(c(X),(a(X),b(X))).
-cl(b(X),d(X)).
-cl(b(X),c(X)).
+prove(Q,Proofs) :- findall(Q:-Proof,solve(Q,Proof),Proofs).
--- problog/prolog_engine/swi_program.py
+++ problog/prolog_engine/swi_program.py
@@ -84,7 +84,7 @@ class SWIProgram(ProbLogObject):
         new_fact = (i, handle_prob(node.probability), handle_functor(node.functor, node.args))
         self.facts.append(new_fact)
         self.index_dict[i] = new_fact
-        self.prolog.assertz('fa({},{})'.format(self.facts[-1][2], self.facts[-1][1]))
+        self.prolog.assertz('fa({},{})'.format(self.facts[-1][1], self.facts[-1][2]))
 
     def add_clause(self, node):
         i = self.new_entry()
@@ -102,13 +102,15 @@ class SWIProgram(ProbLogObject):
 
     def get_lines(self):
         lines = ['cl({},({}))'.format(c[1], c[2]) for c in self.clauses]
-        lines += ['fa({},{})'.format(c[2],c[1]) for c in self.facts]
+        lines += ['fa({},{})'.format(c[1], c[2]) for c in self.facts]
         return lines
 
     def __str__(self):
         return '\n'.join(l + '.' for l in self.get_lines())
 
     def build_formula(self, proof, target):
+        if not hasattr(target, 'd'):
+            target.d = dict()
         t = proof.functor
         if t == ':-':
             name, body = proof.args
@@ -120,23 +122,32 @@ class SWIProgram(ProbLogObject):
            # body = term2list(body, False)
             new = target.add_and([self.build_formula(b, target) for b in body])
             return new
+        elif t == ';':
+            body = proof.args
+            new = target.add_or([self.build_formula(b, target) for b in body])
+            return new
         elif t == 'true':
             return target.TRUE
-        elif t == 'p':
-            p = float(proof.args[0])
-            if p > 1.0 - 1e-5:
-                return target.TRUE
-            return target.add_atom(target.get_next_atom_identifier(), p)
+        elif t == '::':
+            name = proof.args[1]
+            if str(name) not in target.d:
+                p = float(proof.args[0])
+                if p > 1.0 - 1e-5:
+                    key = target.TRUE
+                key = target.add_atom(target.get_next_atom_identifier(), p)
+                target.add_name(name, key)
+                target.d[str(name)] = key
+            return target.d[str(name)]
         else:
             raise Exception('Unhandled node type ' + str(proof))
-        return key
 
     def add_proofs(self, proofs, target):
         target.names = dict()
         for proof in proofs:
+            query, proof = proof.args
             key = self.build_formula(proof, target)
             if not target.is_trivial():
-                target.add_name(Term('query'), key, label=target.LABEL_QUERY)
+                target.add_name(query, key, label=target.LABEL_QUERY)
         return target
 
     def query(self, query, profile=0):
