721ded42f6cf95d2403a73f68e30123d87630336
Thomas Winters <info@thomaswinters.be>
2019-11-26 15:51:25 +0100

    Added delta for testing

--- problog/test/test_tasks.py
+++ problog/test/test_tasks.py
@@ -1,24 +1,37 @@
 import unittest
+from collections import OrderedDict
 
 from problog.logic import Term
 from problog.tasks import dtproblog
 
 
 class TestTasks(unittest.TestCase):
-    def dt_problog_check_if_output_equals(self, dt_file, expected_output):
+    def dt_problog_check_if_output_equals(self, dt_file, expected_choices, expected_score, expected_stats):
         real_file_name = "./../../test/dtproblog/" + dt_file
         result = dtproblog.main([real_file_name])
         print("RESULT", dt_file, result)
-        self.assertTrue(result[0])
-        self.assertEqual(expected_output, result[1])
+        choices, score, stats = result[1]
+        self.assertEqual(OrderedDict(expected_choices), OrderedDict(choices))
+        self.assertAlmostEqual(expected_score, score, delta=1e-6)
+        self.assertEqual(expected_stats, stats)
 
-    def test_dt_problog(self):
-        self.dt_problog_check_if_output_equals("ex1.pl", ({Term("b"): 1}, 1.7999999999999998, {"eval": 2}))
-        self.dt_problog_check_if_output_equals("ex2.pl", ({Term("c"): 1}, 1.7999999999999998, {"eval": 2}))
-        self.dt_problog_check_if_output_equals("ex3.pl", ({Term("a"): 1}, 1.7999999999999998, {"eval": 2}))
-        self.dt_problog_check_if_output_equals("ex4.pl", ({Term("b"): 1, Term("c"): 1}, 3, {"eval": 4}))
-        self.dt_problog_check_if_output_equals("ex5.pl", ({Term("a"): 1, Term("c"): 1}, 1.7999999999999998, {"eval": 4}))
+    def test_simple_dt_problog(self):
+        self.dt_problog_check_if_output_equals(
+            "ex1.pl", {Term("b"): 1}, 1.8, {"eval": 2}
+        )
+        self.dt_problog_check_if_output_equals(
+            "ex2.pl", {Term("c"): 1}, 1.8, {"eval": 2}
+        )
+        self.dt_problog_check_if_output_equals(
+            "ex3.pl", {Term("a"): 1}, 1.8, {"eval": 2}
+        )
+        self.dt_problog_check_if_output_equals(
+            "ex4.pl", {Term("b"): 1, Term("c"): 1}, 3, {"eval": 4}
+        )
+        self.dt_problog_check_if_output_equals(
+            "ex5.pl", {Term("a"): 1, Term("c"): 1}, 1.8, {"eval": 4}
+        )
 
 
-if __name__ == '__main__':
-    unittest.main()
+    if __name__ == "__main__":
+        unittest.main()
