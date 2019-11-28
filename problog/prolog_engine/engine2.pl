d6f7ef72311aa486ef420c10bb1e7ef838faf154
Robin Manhaeve <robin.manhaeve@cs.kuleuven.be>
2019-11-28 14:23:23 +0100

    Added foreign to meta-solver

--- problog/prolog_engine/engine2.pl
+++ problog/prolog_engine/engine2.pl
@@ -6,12 +6,9 @@
 solve(true).% :- !.
 solve((A,B)) :-writeln(and(A,B)), solve(A), solve(B),writeln(proven_and(A,B)).
 solve(call(A)) :-  solve(A),recordz(proof,call(A):-A).
-%solve(neg(A)) :- !,\+solve(A).
-%solve(neg(A)) :- writeln(neg(A)),findall(A,solve(A),Proofs),writeln(neg(A)=Proofs),Term=..[','|Proofs],recordz(proof,neg(A):-neg(Term)).% length(Proofs,0).
 solve(neg(A)) :- copy_term(A,A2), forall(solve(A),recordz(proof,neg(A2):-A)).
 solve(A) :- functor(A,F,_), builtin(F),writeln(F=builtin),  A, recordz(proof,A:-builtin(A)).
-%solve(A) :- predicate_property(A, foreign), !, A, recordz(proof,A:-foreign(A)).
-%solve(A) :- findall(B,(cl(A,B),solve(B)),Proofs),forall(member(B,Proofs),recordz(proof,A:-B)).
+solve(A) :- predicate_property(A, foreign), !, A, recordz(proof,A:-foreign(A)).
 solve(A) :- fa(I,P,A),recordz(proof,::(I,P,A)).
 solve(A) :- cl(A,B),solve(B),recordz(proof,A:-B).
 
