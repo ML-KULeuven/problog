2b842efe2c3de5ea63ae09c01fedef8651aa6696
Thomas Winters <info@thomaswinters.be>
2019-11-28 13:41:19 +0100

    Used dict in sample test instead

--- problog/test/test_sample.py
+++ problog/test/test_sample.py
@@ -3,6 +3,8 @@ import random
 import unittest
 from pathlib import Path
 
+from problog.logic import Term
+
 from problog.program import PrologString
 from problog.tasks import sample
 
@@ -17,7 +19,7 @@ class TestSampleTask(unittest.TestCase):
     def get_samples(self, file_name, num_samples=1):
         file_name = test_folder / file_name
         model = PrologString(file_name.read_text())
-        sample_generator = sample.sample(model, n=num_samples)
+        sample_generator = sample.sample(model, n=num_samples, format="dict")
         result_list = list(sample_generator)
         self.assertEqual(
             num_samples,
@@ -27,18 +29,24 @@ class TestSampleTask(unittest.TestCase):
         return result_list
 
     @staticmethod
-    def count_number_of_atoms(sample_list, atom_name):
-        return len([s for s in sample_list if (atom_name + ".") in s])
+    def count_number_of_atoms(sample_list, term):
+        return len(
+            [
+                sampled_dict
+                for sampled_dict in sample_list
+                if term in sampled_dict and sampled_dict[term] is True
+            ]
+        )
 
-    def test_some_heads_through_task_main(self):
+    def test_task_some_heads(self):
         file_name = test_folder / "some_heads.pl"
         result = sample.main([str(file_name), "-n", str(1)])
         success, data = result
         if not success:
             self.fail("Could not successfully sample" + str(file_name))
 
-    def test_some_heads(self):
+    def test_some_heads_distribution(self):
         samples = self.get_samples("some_heads.pl", num_samples=1000)
         self.assertAlmostEqual(
-            800, self.count_number_of_atoms(samples, "someHeads"), delta=50
+            800, self.count_number_of_atoms(samples, Term("someHeads")), delta=50
         )
