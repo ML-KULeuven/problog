d877569a77ed1cd84bb930bf8385d69c9aeff2c5
Thomas Winters <info@thomaswinters.be>
2019-11-28 13:35:24 +0100

    Added sample test to see if it respects probability

--- problog/test/test_sample.py
+++ problog/test/test_sample.py
@@ -4,7 +4,6 @@ import unittest
 from pathlib import Path
 
 from problog.program import PrologString
-
 from problog.tasks import sample
 
 dirname = os.path.dirname(__file__)
@@ -13,13 +12,23 @@ test_folder = Path(dirname, "./../../test/sample")
 
 class TestSampleTask(unittest.TestCase):
     def setUp(self) -> None:
-        random.seed(123)
+        random.seed(12345)
 
     def get_samples(self, file_name, num_samples=1):
         file_name = test_folder / file_name
         model = PrologString(file_name.read_text())
         sample_generator = sample.sample(model, n=num_samples)
-        return list(sample_generator)
+        result_list = list(sample_generator)
+        self.assertEqual(
+            num_samples,
+            len(result_list),
+            "Was not able to sample the right number of samples from " + str(file_name),
+        )
+        return result_list
+
+    @staticmethod
+    def count_number_of_atoms(sample_list, atom_name):
+        return len([s for s in sample_list if (atom_name + ".") in s])
 
     def test_some_heads_through_task_main(self):
         file_name = test_folder / "some_heads.pl"
@@ -29,19 +38,7 @@ class TestSampleTask(unittest.TestCase):
             self.fail("Could not successfully sample" + str(file_name))
 
     def test_some_heads(self):
-        samples = self.get_samples("some_heads.pl", num_samples=100)
-        print("Samples", samples)
-
-        # success = result[0]
-        # if not success:
-        #     self.fail("Failed executing MAP on " + str(file_name))
-        # else:
-        #     choices, score, stats = result[1]
-        #     self.assertEqual(
-        #         {
-        #             Term("edge", Constant(1), Constant(2)): 1,
-        #             Term("edge", Constant(1), Constant(3)): 0,
-        #         },
-        #         choices,
-        #     )
-        #     self.assertAlmostEqual(1.878144, score, places=5)
+        samples = self.get_samples("some_heads.pl", num_samples=1000)
+        self.assertAlmostEqual(
+            800, self.count_number_of_atoms(samples, "someHeads"), delta=50
+        )
