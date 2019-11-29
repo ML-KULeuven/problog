f362d9af9bbfe8a76b6e31e58f6252d0f711c761
Yann Dauxais <yann.dauxais@cs.kuleuven.be>
2019-11-29 17:23:31 +0100

    Improve foreign function decorator

--- problog/prolog_engine/extern.py
+++ problog/prolog_engine/extern.py
@@ -1,35 +1,108 @@
 import sys
-from pyswip import registerForeign, Term as SWITerm, getFloat
+from pyswip import registerForeign, Term as SWITerm, Functor as SWIFunctor, Variable as SWIVariable, getFloat, PL_foreign_context, \
+    PL_foreign_control, PL_FIRST_CALL, PL_REDO, PL_PRUNED, PL_retry, PL_FA_NONDETERMINISTIC
 from problog.logic import Term
-from swip import pyswip_to_term, term_to_pyswip
+from problog.extern import problog_export
+from swip import pyswip_to_term, term_to_pyswip, pyswip_to_number
 
 
 class swi_problog_export(object):
+    database = None
 
     def __init__(self, *args, **kwdargs):
-        self.arity = len(args)
-        self.input_arguments = [a[1:] for a in args if a[0] == '+']
-        self.output_arguments = [a[1:] for a in args if a[0] == '-']
+        if not swi_problog_export.database:
+            swi_problog_export.database = problog_export.database
 
-        self.input_ids = {i for i in range(len(args)) if args[i][0] == '+'}
-        self.output_ids = {i for i in range(len(args)) if args[i][0] == '-'}
+        self.arity = len(args)
+        self.input_arguments = [a[1:] for a in args if a[0] in ('+', '?')]
+        self.output_arguments = [a[1:] for a in args if a[0] in ('-', '?')]
+
+        self.input_ids = {i for i in range(len(args)) if args[i][0] in ('+', '?')}
+        self.output_ids = {i for i in range(len(args)) if args[i][0] in ('-', '?')}
 
     def __call__(self, func, *args, **kwargs):
         def wrapper(*args):
-            inputs = self.__convert_inputs(*args)
-            result = func(*inputs)
-            self.__convert_outputs(result, *args)
-            return True
+            inputs = self._convert_inputs(*args)
+            result = func(*inputs, database=swi_problog_export.database)
+            if result:
+                return self._convert_outputs(result, *args)
+            return False
 
         registerForeign(wrapper, name=func.__name__, arity=self.arity)
         return wrapper
 
-    def __convert_inputs(self, *args):
-        # TODO: Implement a cleaner version of the casting of a SWI-predicate to a Problog Term
-        return [pyswip_to_term(args[i]) for i in self.input_ids]
+    def _convert_inputs(self, *args):
+        x = 0
+        res = []
+        for i in self.input_ids:
+            if self.input_arguments[x] == "int":
+                res.append(pyswip_to_number(args[i], int))
+            elif self.input_arguments[x] == "float":
+                res.append(pyswip_to_number(args[i], float))
+            else:
+                res.append(pyswip_to_term(args[i]))
+            x += 1
+        return res
 
-    def __convert_outputs(self, result, *args):
+    def _convert_outputs(self, result, *args):
         x = 0
         for i in self.output_ids:
-            args[i].unify(term_to_pyswip(result[x]))
+            if self.output_arguments[x] == "int" or self.output_arguments == "float":
+                res = result[x]
+            else:
+                res = term_to_pyswip(result[x])
+            if not swi_problog_export.unify(args[i], res):
+                return False
             x += 1
+        return True
+
+    @staticmethod
+    def unify(term, value):
+        print(term, value)
+        if type(term) is SWIVariable:
+            term.unify(value)
+            return True
+        elif type(term) is SWIFunctor:
+            if term.arity == value.arity:
+                for i in range(term.arity):
+                    r = swi_problog_export.unify(term.args[i], value.args[i])
+                    if not r:
+                        return False
+                return True
+            else:
+                return False
+        else:
+            return term == value
+
+
+class swi_problog_export_nondet(swi_problog_export):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._cached_result = None
+
+    def __call__(self, func, *args, **kwargs):
+        def wrapper(*args):
+            context = args[-1]
+            control = PL_foreign_control(context)
+            context = PL_foreign_context(context)
+
+            if control == PL_FIRST_CALL:
+                context = 0
+                inputs = self._convert_inputs(*args[:-1])
+                self._cached_result = func(*inputs, database=swi_problog_export.database)
+
+            if control != PL_PRUNED:
+                return self._unification(context, *args[:-1])
+
+        registerForeign(wrapper, name=func.__name__, arity=self.arity, flags=PL_FA_NONDETERMINISTIC)
+        return wrapper
+
+    def _unification(self, context, *args):
+        if self._cached_result and len(self._cached_result) > context:
+            if not self._convert_outputs(self._cached_result[context], *args):
+                return self._unification(context+1, *args)
+            context += 1
+            return PL_retry(context)
+        return False
+
