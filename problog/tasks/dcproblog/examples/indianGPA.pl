3b7a6ac07b9245f7bd2eb15cfa9e34255afc8052
pedrozudo <pedrozuidbergdosmartires@gmail.com>
2020-04-16 20:53:08 +0200

    small change in example indiangpa

--- problog/tasks/dcproblog/examples/indianGPA.pl
+++ problog/tasks/dcproblog/examples/indianGPA.pl
@@ -3,17 +3,17 @@
 19/20::isdensity(a):-american.
 99/100::isdensity(i):-indian.
 
-17/20::excellent_gpa(a):-american.
-1/10::excellent_gpa(i):-indian.
+17/20::perfect_gpa(a):-american.
+1/10::perfect_gpa(i):-indian.
 
 
 gpa(a)~uniform(0,4):- isdensity(a).
-gpa(a)~delta(4.0):-\+isdensity(a), excellent_gpa(a).
-gpa(a)~delta(0.0):-\+isdensity(a), \+excellent_gpa(a).
+gpa(a)~delta(4.0):-\+isdensity(a), perfect_gpa(a).
+gpa(a)~delta(0.0):-\+isdensity(a), \+perfect_gpa(a).
 
 gpa(i)~uniform(0,10):- isdensity(i).
-gpa(i)~delta(10.0):-\+isdensity(i), excellent_gpa(i).
-gpa(i)~delta(0.0):-\+isdensity(i), \+excellent_gpa(i).
+gpa(i)~delta(10.0):-\+isdensity(i), perfect_gpa(i).
+gpa(i)~delta(0.0):-\+isdensity(i), \+perfect_gpa(i).
 
 gpa(student)~delta(A):- A is gpa(a).
 gpa(student)~delta(I):- I is gpa(i).
