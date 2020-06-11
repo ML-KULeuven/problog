fa15ca7745f7880061d90154f95cf3eab8b2651e
wenchiyang <Wen-Chi.Yang@cs.kuleuven.be>
2020-06-11 11:20:05 +0200

    refactoring

--- problog/learning/lfi.py
+++ problog/learning/lfi.py
@@ -1013,6 +1013,12 @@ class LFIProblem(LogicProgram):
                     Clause(lfi_par, new_body),
                 ]
 
+                # TODO: replace above with below
+                # extra_clauses += [
+                #     Clause(atom1.with_probability(), lfi_par & lfi_fact),
+                #     Clause(lfi_par, new_body),
+                # ]
+
                 self.append_ad(len(self._weights))
                 # 4) Set initial weight
                 if start_value is None:
@@ -1035,12 +1041,7 @@ class LFIProblem(LogicProgram):
             if len(atoms) == 1:  # Simple clause
                 return [atoms_out[0]] + extra_clauses
             else:
-                if body is None:
-                    return [
-                        AnnotatedDisjunction(atoms_out, Term("true"))
-                    ] + extra_clauses
-                else:
-                    return [AnnotatedDisjunction(atoms_out, body)] + extra_clauses
+                return [AnnotatedDisjunction(atoms_out, lfi_par)] + extra_clauses
         else:
             if len(atoms) == 1:
                 if body is None:
