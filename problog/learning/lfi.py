bfdea2e448cc718fcdfe01d88798b25431c5ac1c
Arcchit Jain <arcchitjain@users.noreply.github.com>
2020-07-22 18:42:52 +0200

    Keeping output format consistent with the master and web-interface

--- problog/learning/lfi.py
+++ problog/learning/lfi.py
@@ -100,7 +100,7 @@ from problog.logic import (
     term2list,
 )
 from problog.program import PrologString, PrologFile, LogicProgram
-from problog.errors import InconsistentEvidenceError
+from problog.errors import InconsistentEvidenceError, process_error
 from problog import get_evaluatable, get_evaluatables
 import traceback
 
@@ -1420,13 +1420,10 @@ class DefaultDict(object):
         return self.base.get(key, Var(key))
 
 
-def run_lfi(program, examples, output_model=None, **kwdargs):
+def run_lfi(program, examples, **kwdargs):
     lfi = LFIProblem(program, examples, **kwdargs)
     score = lfi.run()
 
-    if output_model is not None:
-        with open(output_model, "w") as f:
-            f.write(lfi.get_model())
     getLogger("problog_lfi").info("\nLearned Model:\t\n" + lfi.get_model())
 
     names = []
@@ -1453,7 +1450,8 @@ def argparser():
     parser.add_argument("-d", dest="min_improv", default=1e-10, type=float)
     parser.add_argument(
         "-o",
-        "--output-model",
+        "--output",
+        dest="output",
         type=str,
         default=None,
         help="write resulting model to given file",
@@ -1550,12 +1548,17 @@ def main(argv, result_handler=None):
 
     knowledge = get_evaluatable(args.koption)
 
-    if args.logger is None:
+    if args.output is None:
         outf = None
     else:
-        outf = open(args.logger, "w")
+        outf = open(args.output, "w")
 
-    logger = init_logger(verbose=args.verbose, name="problog_lfi", out=outf)
+    if args.logger is None:
+        logf = None
+    else:
+        logf = open(args.logger, "w")
+
+    logger = init_logger(verbose=args.verbose, name="problog_lfi", out=logf)
     create_logger("problog_lfi", args.verbose - 1)
 
     program = PrologFile(args.model)
@@ -1580,6 +1583,9 @@ def main(argv, result_handler=None):
         retcode = result_handler((False, err), outf=outf)
 
     if args.logger is not None:
+        logf.close()
+
+    if args.output is not None:
         outf.close()
 
     if retcode:
@@ -1590,10 +1596,11 @@ def print_result(d, outf, precision=8):
     success, d = d
     if success:
         score, weights, names, iterations, lfi = d
-        # print(score, weights, names, iterations, file=outf)
+        weights = list(map(lambda x: round(x, precision), weights))
+        print(score, weights, names, iterations, file=outf)
         return 0
     else:
-        # print(process_error(d), file=outf)
+        print(process_error(d), file=outf)
         return 1
 
 
@@ -1613,10 +1620,10 @@ def print_result_json(d, outf, precision=8):
             ],
             "model": lfi.get_model(),
         }
-        # print(json.dumps(results), file=outf)
+        print(json.dumps(results), file=outf)
     else:
         results = {"SUCCESS": False, "err": vars(d)}
-        # print(json.dumps(results), file=outf)
+        print(json.dumps(results), file=outf)
     return 0
 
 
